c55d20ffdf823c8607cfb1990652891385a8ad02
BingQ <bingbing13@163.com>
2019-09-26 00:58:17 -0700

    🐒

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+p '🐒 creating seeds....'
 3.times do
   u = User.create!(email: Faker::Internet.email,
                       password: '123456')
